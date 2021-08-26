e58b121ff982ea1a22086959d562bb5522bda4d9
rpcme <rich@richelberger.com>
2021-08-26 16:25:52 -0400

    add new prefix param to ensure uniqueness for ECR repos

--- core/scripts/setup_ci_container_poky.sh
+++ core/scripts/setup_ci_container_poky.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 prefix=$1
 dockerhub_secret_arn=$2
-
+set +x
 if test $# -ne 2; then
     echo you must pass in 2 arguments: prefix and dockerhub secret arn  
     exit 1
@@ -11,16 +11,16 @@ echo invoking the template.
 
 URL=https://${prefix}-el-cloudformation-staging.s3.amazonaws.com/ci_container_poky.yml
 STACKNAME=${prefix}-el-ci-container-poky
+PREFIX_PARAM=ParameterKey=Prefix,ParameterValue=${prefix}
 NETWORK_STACK_NAME=ParameterKey=NetworkStackName,ParameterValue=${prefix}-el-ci-network
 DOCKERHUB_SECRET_ARN=ParameterKey=DockerhubSecretArn,ParameterValue=${dockerhub_secret_arn}
 
-stack_id=$(aws cloudformation create-stack --output text \
+stack_id=$(aws cloudformation create-stack --output text --query StackId \
                --stack-name ${STACKNAME} \
                --template-url "${URL}" \
                --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
-               --parameters "${NETWORK_STACK_NAME}" \
-               --parameters "${DOCKERHUB_SECRET_ARN}" \
-               --query StackId)
+               --parameters ${NETWORK_STACK_NAME} ${DOCKERHUB_SECRET_ARN} ${PREFIX_PARAM}
+               )
 
 echo stack_id is [${stack_id}]
 deployment_status=CREATE_IN_PROGRESS
