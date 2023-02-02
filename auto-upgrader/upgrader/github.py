9039edc2e50da8d9112f5262e26d9f87f02c4032
Thomas Roos <throos@amazon.de>
2023-02-02 15:45:43 +0100

    auto-upgrader: add pull request delay

--- auto-upgrader/upgrader/github.py
+++ auto-upgrader/upgrader/github.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import time
 from pathlib import Path
 from typing import Optional, Sequence
 
@@ -13,9 +14,10 @@ logger = logging.getLogger(__name__)
 @click.option("--branch-file", type=click.Path(exists=True))
 @click.option("--target-branch", type=str)
 @click.option("--repo", type=str)
+@click.option("--delay", type=int)
 @click.argument("branches", nargs=-1)
 def create_pulls(
-    target_branch: str, repo: str, branch_file: Optional[Path], branches: Sequence[str]
+    target_branch: str, repo: str, delay: Optional[int], branch_file: Optional[Path], branches: Sequence[str]
 ) -> None:
     """
     Create pull requests.
@@ -30,6 +32,9 @@ def create_pulls(
     branch_file : Optional[Path]
         The path to a file which lists branches to create pull requests
         for. Mutually exclusive with branches
+    delay : Optional[int]
+        Sleep in seconds after each pull request creation to not hit GitHub api rate limit
+        and give sstate server time to be filled by the first build.
     branches : Iterable[str]
         A list of branches to create pull requests for. Mutually
         exclusive with branch_file.
@@ -42,10 +47,10 @@ def create_pulls(
         logger.info("no branches to create pulls for")
     else:
         logger.info(f"creating pulls for branches {branches}")
-        _create_prs(target_branch, repo, branches)
+        _create_prs(target_branch, repo, delay, branches)
 
 
-def _create_prs(target_branch: str, repo: str, branches: Sequence[str]) -> None:
+def _create_prs(target_branch: str, repo: str, delay: int, branches: Sequence[str]) -> None:
     assert "GITHUB_TOKEN" in os.environ, "GITHUB_TOKEN not found in env"
     token = os.environ.get("GITHUB_TOKEN")
     gh = Github(token)
@@ -56,3 +61,5 @@ def _create_prs(target_branch: str, repo: str, branches: Sequence[str]) -> None:
             base=target_branch, head=branch, title=branch, body="Automatically created."
         )
         pull.set_labels(upgrade_label)
+        if delay:
+            time.sleep(delay)
