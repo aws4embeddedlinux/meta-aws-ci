54e04d89e9f274b30ddb9b52b83aad55947e1ce2
glimsdal <glimsdal@amazon.com>
2023-06-28 12:56:27 -0500

    add basic test for build-image-data stack

---
+++ demos-pipeline/test/build-image-data.test.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { BuildImageDataStack } from '../lib/build-image-data';
+
+test('S3 Bucket Has Versioning Enabled', () => {
+    const props = { bucketName: 'test-bucket' };
+    const app = new cdk.App();
+    // WHEN
+    const stack = new BuildImageDataStack(app, 'MyTestStack', props);
+    // THEN
+    const template = Template.fromStack(stack);
+    template.hasResourceProperties('AWS::S3::Bucket', {
+        BucketName: 'test-bucket',
+    });
+
+    template.allResourcesProperties('AWS::S3::Bucket', {
+        VersioningConfiguration: { Status: "Enabled" },
+    });
+});
