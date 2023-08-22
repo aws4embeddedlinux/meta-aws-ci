6caf09976b72cfc1cad4c039e7372b9ae8a80e59
glimsdal <glimsdal@amazon.com>
2023-08-22 13:48:56 -0700

    add snapshot test for BuildImageData

--- demos-pipeline/lib/test/build-image-data.test.ts
+++ demos-pipeline/lib/test/build-image-data.test.ts
@@ -2,18 +2,29 @@ import * as cdk from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
 import { BuildImageDataStack } from "../lib/build-image-data";
 
-test("S3 Bucket Has Versioning Enabled", () => {
-  const props = { bucketName: "test-bucket" };
-  const app = new cdk.App();
-  // WHEN
-  const stack = new BuildImageDataStack(app, "MyTestStack", props);
-  // THEN
-  const template = Template.fromStack(stack);
-  template.hasResourceProperties("AWS::S3::Bucket", {
-    BucketName: "test-bucket",
+describe("Build Image Data", () => {
+  const props = {
+    bucketName: "test-bucket",
+    env: { account: "12341234", region: "eu-central-1" },
+  };
+
+  test("S3 Bucket Has Versioning Enabled", () => {
+    const app = new cdk.App();
+    const stack = new BuildImageDataStack(app, "MyTestStack", props);
+    const template = Template.fromStack(stack);
+    template.hasResourceProperties("AWS::S3::Bucket", {
+      BucketName: "test-bucket",
+    });
+
+    template.allResourcesProperties("AWS::S3::Bucket", {
+      VersioningConfiguration: { Status: "Enabled" },
+    });
   });
 
-  template.allResourcesProperties("AWS::S3::Bucket", {
-    VersioningConfiguration: { Status: "Enabled" },
+  test("Build Image Snapshot", () => {
+    const app = new cdk.App();
+    const stack = new BuildImageDataStack(app, "MyTestStack", props);
+    const template = Template.fromStack(stack);
+    expect(template).toMatchSnapshot();
   });
 });
