a731573218db7d563697f5b7cc0fa992490c1453
tro <throos@amazon.de>
2024-01-16 13:49:49 +0100

    meta-aws-tests.sh: add THINGS_TO_EXCLUDE
    THINGS_TO_EXCLUDE variable added that will exclude files found for ptest execution

--- meta-aws-tests/meta-aws-tests.sh
+++ meta-aws-tests/meta-aws-tests.sh
@@ -147,8 +147,11 @@ for RELEASE in $RELEASES ; do
     # find all recipes in meta-aws or use package
     ALL_RECIPES=${PACKAGE-`find ../meta-aws -name *.bb -type f  | sed 's!.*/!!' | sed 's!.bb!!' | sed 's!_.*!!' | sort | uniq | sed -z 's/\n/ /g'`}
 
+    # those tests are known to not work with others etc.
+    THINGS_TO_EXCLUDE="-name aws-cli_*"
+
     # find all recipes having a ptest in meta-aws
-    ptest_recipes=${PACKAGE-`find ../meta-aws -name *.bb -type f -print | xargs grep -l 'inherit.*ptest.*'| sed 's!.*/!!' | sed 's!.bb!!' | sed 's!_.*!!' | sort | uniq | sed -z 's/\n/ /g'`}
+    ptest_recipes=${PACKAGE-`find ../meta-aws -name *.bb -type f ! \( ${THINGS_TO_EXCLUDE} \) -print | xargs grep -l 'inherit.*ptest.*'| sed 's!.*/!!' | sed 's!.bb!!' | sed 's!_.*!!' | sort | uniq | sed -z 's/\n/ /g'`}
 
     # make array out of string
     ptest_recipes_array=($(echo "$ptest_recipes" | tr ',' '\n'))
